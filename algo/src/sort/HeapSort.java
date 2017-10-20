94890a0f35e948ceb694789cb6a9947d7b9d8686
Tanglong <13685759344@163.com>
2017-10-20 23:39:52 +0800

    Heap Sort

---
+++ algo/src/sort/HeapSort.java
@@ -0,0 +1,86 @@
+/**
+ * 在最坏情况下，时间复杂度O(NlogN)
+ *
+ * 实施过程：
+ * 1，新建堆
+ * 2，重建堆
+ */
+
+package sort;
+
+import java.util.Random;
+
+public class HeapSort {
+	public static void main(String[] args) {
+		int [] intArr = new int [CommonFinal.LENGTH];
+		//堆排序
+		//数据初始化
+		Random rd = new Random();
+		for(int i=0;i<CommonFinal.LENGTH;i++){
+			intArr[i]=rd.nextInt(CommonFinal.RANGE);
+		}
+		//排序前
+		System.out.println("排序前:");
+		for(int i : intArr){
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		long begin = System.nanoTime();
+		//堆排序
+		heapSort(intArr);
+		long end = System.nanoTime();
+		System.out.printf("堆排序共耗时%f纳秒%n",(end-begin)/1.0);
+		//堆排序排序后
+		System.out.println("堆排序后:");
+		for(int i : intArr){
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	public static  void heapSort(int[] a){
+		int len=a.length;
+		//循环建堆
+		for(int i=0;i<len-1;i++){
+			//建堆
+			buildMaxHeap(a,len-1-i);
+			//交换堆顶和最后一个元素
+			swap(a,0,len-1-i);
+		}
+	}
+	private static  void swap(int[] data, int i, int j) {
+		int tmp=data[i];
+		data[i]=data[j];
+		data[j]=tmp;
+	}
+	//对data数组从0到lastIndex建大顶堆
+	private static void buildMaxHeap(int[] data, int lastIndex) {
+		//从lastIndex处节点（最后一个节点）的父节点开始
+		for(int i=(lastIndex-1)/2;i>=0;i--){
+			//k保存正在判断的节点
+			int k=i;
+			//如果当前k节点的子节点存在
+			while(k*2+1<=lastIndex){
+				//k节点的左子节点的索引
+				int biggerIndex=2*k+1;
+				//如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+				if(biggerIndex<lastIndex){
+					//若果右子节点的值较大
+					if(data[biggerIndex]<data[biggerIndex+1]){
+						//biggerIndex总是记录较大子节点的索引
+						biggerIndex++;
+					}
+				}
+				//如果k节点的值小于其较大的子节点的值
+				if(data[k]<data[biggerIndex]){
+					//交换他们
+					swap(data,k,biggerIndex);
+					//将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+					k=biggerIndex;
+				}else{
+					break;
+				}
+			}
+		}
+	}
+}
