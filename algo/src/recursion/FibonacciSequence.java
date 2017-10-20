4b47191836c35fbabfb618084a5f78ca973ef8d4
Tanglong <13685759344@163.com>
2017-10-20 20:57:24 +0800

    Recursion

---
+++ algo/src/recursion/FibonacciSequence.java
@@ -0,0 +1,28 @@
+/**
+ * Formula:F(n|n>2)=F(n-1)+F(n-2)
+ * F(1)=1,F(2)=1
+ */
+
+package recursion;
+
+public class FibonacciSequence {
+	static final int N = 10;//数组长度
+	static final int RANGE = 30;//数据范围
+	public static void main(String[] args) {
+		int n = 0;
+		for(int i=0;i<N;i++){
+			n = (int)(Math.random()*RANGE + 1);
+			System.out.printf("Fabonacci[%d]=%d%n",n,Fabonacci(n));
+		}
+	}
+
+	/**
+	 * Fabonacci
+	 */
+	static int Fabonacci(int n){
+		if(1 == n || 2 == n) {
+			return 1;
+		}
+		return Fabonacci(n-1) + Fabonacci(n-2);
+	}
+}
---
+++ algo/src/recursion/HanoiTower.java
@@ -0,0 +1,31 @@
+/**
+ * 将挂柱A上的n个卡片顺序不变的移动到挂柱C
+ */
+
+package recursion;
+
+import java.util.Scanner;
+
+public class HanoiTower {
+	public static void main(String[] args) {
+		int num;
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入需要移动的卡片数目：");
+		num = scanner.nextInt();
+		hanoi('A','C','B',num);
+	}
+
+	/**
+	 * 汉诺塔移动操作
+	 */
+	static void hanoi(char from,char to,char temp,int n){
+		if(1 == n){
+			System.out.println(from + " move to " + to);
+		}else{
+			hanoi(from,temp,to,n-1);
+			System.out.println(from + " move to " + to);
+			hanoi(temp,to,from,n-1);
+		}
+	}
+}
