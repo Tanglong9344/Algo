4bfe35f1bce74446ba057d24aa1c4c512fdf1856
Tanglong <13685759344@163.com>
2017-11-23 16:32:23 +0800

    Binary Search

--- algo/src/search/BinarySearch.java
+++ algo/src/search/BinarySearch.java
@@ -1,5 +1,59 @@
 package search;
 
-public class BinarySearch {
+import java.util.Arrays;
+import java.util.Random;
 
+/**
+ * 二分查找
+ * @author 唐龙
+ */
+public class BinarySearch {
+	final static int N = 100000;//数据数目
+	final static int R = 1000;//数据范围
+	public static void main(String[] args) {
+		//初始化数据
+		int[] searchTable = new int[N+1];
+		Random random = new Random();
+		for(int i=1;i<N+1;i++){
+			searchTable[i] = random.nextInt(R);
+		}
+		//从小到大排序
+		Arrays.sort(searchTable);
+		System.out.println("查找表："+Arrays.toString(searchTable));
+		//目标数据
+		int key = searchTable[random.nextInt(N+1)];
+		int pos = binarySearch(searchTable,key);
+		if(0 == pos){
+			System.out.println("未找到目标数据"+key);
+		}else{
+			System.out.println("目标数据"+key+"在数组中的位置是"+pos);
+		}
+	}
+
+	/**
+	 * 二分查找算法
+	 * @param searchTable
+	 * @param key
+	 * @return 返回目标数据在查找表中的位置
+	 */
+	static int binarySearch(int[] searchTable,int key){
+		int low = 1;
+		int high = searchTable.length-1;
+		int mid;
+		int times = 0;//统计查找次数
+		while(low<=high){
+			times++;
+			mid = (low + high)/2;
+			if(key == searchTable[mid]){
+				System.out.println("查找次数："+times);
+				return mid;//找到数据
+			}else if(key < searchTable[mid]){
+				high = mid - 1;
+			}else{
+				low = mid + 1;
+			}
+		}
+		System.out.println("查找次数："+times);
+		return 0;//未找到数据
+	}
 }
