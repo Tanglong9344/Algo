b6a163c6d1b35cfa65d52c90b1221cb71467c81d
Tanglong <13685759344@163.com>
2017-11-23 22:12:15 +0800

    顺序查找

--- algo/src/search/OrdinalSearch.java
+++ algo/src/search/OrdinalSearch.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * 二分查找
- * 只适用于顺序存储结构的顺序表
+ * 顺序查找
+ * 无序查找
  * @author 唐龙
  */
-public class BinarySearch {
+public class OrdinalSearch {
 	final static int N = 100000;//数据数目
 	final static int R = 10000;//数据范围
 	final static int CNT = 100;//操作次数
@@ -19,8 +19,6 @@ public class BinarySearch {
 		for(int i=1;i<N+1;i++){
 			searchTable[i] = random.nextInt(R);
 		}
-		//从小到大排序
-		Arrays.sort(searchTable);
 		System.out.println("查找表："+Arrays.toString(searchTable));
 		int key;//目标数据
 		int pos;//数据位置
@@ -43,26 +41,19 @@ public class BinarySearch {
 	}
 
 	/**
-	 * 二分查找算法
+	 * 无序查找算法
 	 * @param searchTable
 	 * @param key
 	 * @return 返回目标数据在查找表中的位置
 	 */
 	static int binarySearch(int[] searchTable,int key){
-		int low = 1;
-		int high = searchTable.length-1;
-		int mid;
-		int times = 0;//统计查找次数
-		while(low<=high){
+		int len = searchTable.length;
+		int times = 0;
+		for(int i=0;i<len;i++){
 			times++;
-			mid = (low + high)/2;
-			if(key == searchTable[mid]){
+			if(key == searchTable[i]){
 				System.out.println("查找次数："+times);
-				return mid;//找到数据
-			}else if(key < searchTable[mid]){
-				high = mid - 1;
-			}else{
-				low = mid + 1;
+				return i;//找到数据
 			}
 		}
 		System.out.println("查找次数："+times);
