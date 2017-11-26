1b4b06b5697f05b2976f44be8b34d8b87d6153d7
Tanglong <13685759344@163.com>
2017-11-26 21:33:50 +0800

    String Matching

--- algo/src/string_matching/StringMatching2.java
+++ algo/src/string_matching/StringMatching2.java
@@ -1,9 +1,9 @@
 package string_matching;
 
 /**
- * 朴素的字符串匹配(模式匹配)算法
+ * 朴素的字符串匹配(模式匹配)算法2
  */
-public class StringMatching {
+public class StringMatching2 {
 	public static void main(String[] args) {
 		String is = stringMatching(CommonString.source,CommonString.dest)?"":"不";
 		System.out.println(CommonString.source+is+"包含 "+CommonString.dest);
@@ -18,20 +18,18 @@ public class StringMatching {
 			return false;
 		}
 		//判断
-		int index,i,j;
-		//source匹配
-		for(i=0;i<sLen-dLen+1;i++){
-			index=i;
-			//dest匹配
-			for(j=0;j<dLen;j++) {
-				if(source.charAt(index++)!=dest.charAt(j)){
-					break;
-				}
-			}
-			if(j>=dLen) {
-				return true;
+		int i=0,j=0;//初始比较位置
+		//如果双方的某个字 符相等则双方继续比较下一个字符
+		//否则dest返回并继续和source的下一个字符比较
+		while(i<sLen && j<dLen){
+			if(source.charAt(i) == dest.charAt(j)){
+				i++;
+				j++;
+			}else{
+				j=0;
+				i=i-j+1;
 			}
 		}
-		return false;
+		return j>=dLen?true:false;
 	}
 }
