7122bd84647b3dc8f4f6c37c4402c1cb9c5a0d85
Tanglong <13685759344@163.com>
2017-11-27 21:09:20 +0800

    Palindrome

--- algo/src/string_matching/Palindrome.java
+++ algo/src/string_matching/Palindrome.java
@@ -1,19 +1,32 @@
+package string_matching;
+
 /**
  * 回文数判断
  */
-
-package string_matching;
-
 public class Palindrome {
 	public static void main(String[] args) {
 		String str="12qwertrewq21";
 		//回文数判断
-		String is = palindromeJudge(str)?"":"不";
-		System.out.println(str+is+"是回文数。");
+		String is = palindromeJudgeHt(str) ? "" : "不";
+		System.out.println(str+is+"是回文数");
 	}
 
-	// palindrome Judgement
-	static boolean palindromeJudge(String str){
+	// palindrome Judgement(head to tail)
+	static boolean palindromeJudgeHt(String str){
+		char[] chars = str.toCharArray();
+		//字符串前半部分和后半部分比较
+		for(int i=0,j=str.length()-1;i<j;i++,j--){
+			if(chars[j] != chars[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// palindrome JudgementRev(Reverse Array)
+	// This is not a good method
+	// just a way to other thought
+	static boolean palindromeJudge2(String str){
 		int len = str.length();
 		char[] chars = str.toCharArray();
 		char[] charsReverse = new char[len];
