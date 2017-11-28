93f59214e47e43be864dcc65f1f923aea3f0825b
Tanglong <13685759344@163.com>
2017-11-28 16:22:25 +0800

    二叉排序树

---
+++ algo/src/search/BinarySortTree.java
@@ -0,0 +1,12 @@
+package search;
+
+/**
+ * 二叉排序树(Binary Sort Tree)
+ * 递归定义：
+ * 1.左子树上所有结点的值均小于它根节点的值
+ * 2.右子树上所有结点的值均大于它根节点的值
+ *
+ * 中序遍历二叉排序树可得到一个关键字的有序序列
+ * @author 唐龙
+ */
+public class BinarySortTree {}
