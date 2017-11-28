017fe6aaff1a8882aa9200c6ed2f44d4594d68b5
Tanglong <13685759344@163.com>
2017-11-28 16:22:38 +0800

    平衡二叉树

---
+++ algo/src/search/BalancedBinaryTree.java
@@ -0,0 +1,15 @@
+package search;
+
+/**
+ * 平衡二叉树(Balanced Binary Tree)
+ * 二叉排序树经过平衡化转化为平衡二叉树
+ * 递归定义：
+ * 1.左右子树均为平衡二叉树
+ * 2.左右子树深度之差的绝对值不超过1
+ *
+ * 平衡因子(Balance Factor)
+ * 结点的左子树深度减去右子树深度
+ * 取值：-1,1,0
+ * @author 唐龙
+ */
+public class BalancedBinaryTree {}
