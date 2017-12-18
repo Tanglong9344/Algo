c2a665a808101699a0501f7688b3ebf9035d068e
Tanglong <13685759344@163.com>
2017-12-18 10:06:36 +0800

    Square Digits

---
+++ algo/src/math/SquareDigit.java
@@ -0,0 +1,26 @@
+package math;
+
+/**
+ * Square each digit in a number
+ *
+ * @author 唐龙
+ *
+ */
+public class SquareDigit {
+	public static void main(String[] args) {
+		System.out.println(squareDigits(12));
+		System.out.println(squareDigits(38972));
+	}
+
+	/**Square digits*/
+	static int squareDigits(int n){
+		String str = String.valueOf(n);
+		StringBuilder sb = new StringBuilder();
+		int len = str.length();
+		for(int i=0;i<len;i++){
+			int tmp = Integer.valueOf(str.charAt(i) + "");
+			sb.append(tmp * tmp + "");
+		}
+		return Integer.valueOf(sb.toString());
+	}
+}
