386f446d0ff8b1d2daaec9a1b51249d4c3e97e97
Tanglong <13685759344@163.com>
2018-03-06 14:44:51 +0800

    Tail recursion

---
+++ algo/src/recursion/TailRecursion.java
@@ -0,0 +1,47 @@
+package recursion;
+
+/**
+ * Common recursion vs Tail recursion
+ *
+ * @author tanglong
+ *
+ */
+public class TailRecursion {
+	public static void main(String[] args) {
+		int n = (int)(Math.random() * 15 + 5);
+		long start, end;
+		long result = -1;
+
+		start = System.nanoTime();
+		result = commonRecursion(n);
+		end = System.nanoTime();
+		System.out.println("Common Recursion:%d%nResult:" + n);
+		System.out.println("Result: " + result + "\nTime: " + (end - start));
+
+		start = System.nanoTime();
+		result = tailRecursion(n, 1);
+		end = System.nanoTime();
+		System.out.println("Tail   Recursion:%d%nResult:" + n);
+		System.out.println("Result: " + result + "\nTime: " + (end - start));
+	}
+
+	/** common recursion
+	 * buffer data using stack
+	 */
+	private static long commonRecursion(int n) {
+		if(n<=0) {
+			return -1;
+		}
+		return n==1? 1 : n * commonRecursion(n-1);
+	}
+
+	/** tail recursion
+	 * storage data using argument
+	 */
+	private static long tailRecursion(int n, long a) {
+		if(n<=0) {
+			return -1;
+		}
+		return n==1? a : tailRecursion(n-1, n * a);
+	}
+}
